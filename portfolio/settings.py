69307078e1f27e64b484cc7ecd867f6d07488cc4
MajorManson <52643263+MajorManson@users.noreply.github.com>
2022-05-16 12:12:10 -0400

    Add comment on env

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -18,6 +18,7 @@ import environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Simply add an .env file to the same dir as settings.py
 env = environ.Env()
 environ.Env.read_env()
 
